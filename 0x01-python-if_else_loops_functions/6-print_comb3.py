3d6f27ce25152e278481f6211099fc720e2d0249
Salma2486 <saloom_001@hotmail.com>
2023-10-31 10:16:45 -0700

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for first in range(10):
-    for second in range(1, 10):
+    for second in range(first + 1, 10):
         if first != second:
             print("{:02d}, ".format(first * 10 + second), end='')
 print("{:02d}".format(98))
