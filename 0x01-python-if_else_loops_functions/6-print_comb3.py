563d5e524ee1285c67224bcfda50f7e0d343880a
Salma2486 <saloom_001@hotmail.com>
2023-10-31 10:34:40 -0700

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for first in range(10):
     for second in range(first + 1, 10):
-        if first != second:
-            print("{:02d}, ".format(first * 10 + second), end='')
-print("{:02d}".format(98))
+        if first == 8 and second == 9:
+            print("{}{}, ".format(first, second))
+        else:
+            print("{}{}".format(first, second), end=", ")
