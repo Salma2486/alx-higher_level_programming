d82a8cc606e40748d95acca74fdd05780b69e899
Salma2486 <saloom_001@hotmail.com>
2023-11-02 05:38:49 -0700

    task3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    total = 0
+    import sys
+    length = len(sys.argv)
+    for i in range(1, length):
+        total += int(sys.argv[i])
+    print(total)
