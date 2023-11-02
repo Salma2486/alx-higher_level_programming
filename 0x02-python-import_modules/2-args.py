052aaee301f8ccfa37fb1288f8ae4dd58d298823
Salma2486 <saloom_001@hotmail.com>
2023-11-02 04:34:52 -0700

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    l = len(sys.argv) - 1
+    if l == 0:
+        print("0 arguments.")
+    elif l == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(l))
+    for i in range(1, l + 1):
+        print("{}: {}".format(i, sys.argv[i]))
