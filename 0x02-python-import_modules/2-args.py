a8ddf317ec7a096ad23ec99a1252bbb0bc169243
Salma2486 <saloom_001@hotmail.com>
2023-11-02 04:58:43 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys
 
     length = len(sys.argv)
-    if l == 1:
+    if length == 1:
         print("{} arguments.".format(length - 1))
-    elif l == 2:
+    elif length == 2:
         print("{} argument:".format(length - 1))
     else:
         print("{} arguments:".format(length - 1))
-    for i in range(1, l):
+    for i in range(1, length):
         print("{}: {}".format(i, sys.argv[i]))
