f55fea15221e518e41a84acca30f5b0dc6398bc7
Salma2486 <saloom_001@hotmail.com>
2023-11-21 12:11:03 -0800

    task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,6 +3,7 @@
 This module contains utility functions for handling strings.
 """
 
+
 class Square:
     """
     A class representing a square.
@@ -11,6 +12,7 @@ class Square:
         __size (int): The size of the square.
         __position (tuple): The position of the square.
     """
+
     def __init__(self, size=0, position=(0, 0)):
         """
         Initializes a new Square instance with the given size and position.
