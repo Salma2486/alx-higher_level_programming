5bf4dce775e9ad673d6a2be9166fb7c6f86b462e
Salma2486 <saloom_001@hotmail.com>
2023-11-21 10:09:26 -0800

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" ksjdrglihsjerhgsil"""
+
+
+class Square:
+    """ejhriwurethwrut"""
+    def __init__(self,size):
+        """ kjserngijwren trewnijlgt"""
+        self.__size = size
