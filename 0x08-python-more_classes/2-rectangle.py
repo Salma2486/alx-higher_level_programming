5ee311e477c884c138b8b993b0aeeb92421c6407
Salma2486 <saloom_001@hotmail.com>
2023-11-27 06:29:23 -0800

    task2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -48,6 +48,6 @@ class Rectangle:
 
     def perimeter(self):
         """uqiot5 uiwe thiue thgu"""
-        if self.__width or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return 2 *(self.__width + self.__height)
