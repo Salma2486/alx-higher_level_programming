960abec4130c8119a04fbc2c1316ee8fbc3c4009
Salma2486 <saloom_001@hotmail.com>
2023-12-04 05:16:52 -0800

    task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""trh edty hjety """
+
+
+class MyList(list):
+    """rstjg uistdg hskjdt gh hseikgs"""
+
+    def print_sorted(self):
+        """uisrt hgusir thsrui ht"""
+        print(sorted(self))
