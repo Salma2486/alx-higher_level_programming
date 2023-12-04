642a5e9592cd28c5785555f740b6f52e8367cb7f
Salma2486 <saloom_001@hotmail.com>
2023-12-04 06:51:36 -0800

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""oiueh truir5 lhyw54i4eul hyeuri yt"""
+
+
+class BaseGeometry:
+    """oseihr gi osrt yilhhsrty oihjwrty"""
+
+    def area(self):
+        """iu srhoih seyoi j"""
+        raise Exception("area() is not implemented")
