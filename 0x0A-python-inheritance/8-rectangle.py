4601c864e4da0a47e78d727f20c121256f474e4b
Salma2486 <saloom_001@hotmail.com>
2023-12-04 07:42:38 -0800

    iuagsfouaewrsg

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """tijose goijest rjip ew"""
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
