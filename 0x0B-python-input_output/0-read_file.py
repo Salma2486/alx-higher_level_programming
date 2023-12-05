c658bc9901a4de2e3e7bf0fd5955199f6e242a30
Salma2486 <saloom_001@hotmail.com>
2023-12-05 05:22:49 -0800

    task0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""oier tojner toijet """
+
+
+def read_file(filename=""):
+    """ijow5t oijw45 yoijw4 5y"""
+    with open('filename', 'r') as file:
+        file_data = file.read()
+        print(file_data)
