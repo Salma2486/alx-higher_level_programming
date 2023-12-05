6fdf599987b21d3ebf0af9851424e0cc36b5a7f4
Salma2486 <saloom_001@hotmail.com>
2023-12-05 05:45:12 -0800

    task0

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""tyf  67yitfu  tfyu fk"""
+
+
+def append_write(filename="", text=""):
+    """iu 4wyoin wy46inlow y64"""
+        with open(filename, 'a', encoding="utf-8") as file:
+            return file.write(text)
