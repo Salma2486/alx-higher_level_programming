8656a675fd97d0e60cfab5d06a202d3c598f2595
Salma2486 <saloom_001@hotmail.com>
2023-12-05 06:47:44 -0800

    task4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""l ieysh5ohi wy5iojs yrilsy y s"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """oijrdyijoir dyioj6 urdioj rusr rsu"""
+    return json.loads(my_str)
