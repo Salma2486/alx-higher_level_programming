9bab5fd7d47c60f59b3d2f6a151a395b96a5db49
Salma2486 <saloom_001@hotmail.com>
2023-12-05 07:39:09 -0800

    aerfqergf

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,17 +3,17 @@
 save them to a file
 """
 import sys
-import os.path
 
 
 save_file = __import__('7-save_to_json_file').save_to_json_file
 load_file = __import__('8-load_from_json_file').load_from_json_file
 
-my_list = []
-if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
+arglist = list(sys.argv[1:])
 
-for arg in sys.argv[1:]:
-    my_list.append(arg)
+try:
+    old_data = load_from_json_file('add_item.json')
+except Exception:
+    old_data = []
 
-save_file(my_list, "add_item.json")
+old_data.extend(arglist)
+save_to_json_file(old_data, 'add_item.json')
