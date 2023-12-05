c39487719faa4b9f8ce67c8246c1d1339a56ea4c
Salma2486 <saloom_001@hotmail.com>
2023-12-05 07:22:01 -0800

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,18 +2,29 @@
 """ Module that adds all arguments to a Python list, and then
 save them to a file
 """
+
 import sys
-import os.path
+import json
 
 
-save_file = __import__('7-save_to_json_file').save_to_json_file
-load_file = __import__('8-load_from_json_file').load_from_json_file
+def save_to_json_file(my_obj, filename):
+    """ y6re u udr u6d u"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(my_obj, file, ensure_ascii=False, indent=2)
 
-my_list = []
-if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
 
-for arg in sys.argv[1:]:
-    my_list.append(arg)
+def load_from_json_file(filename):
+    """ yw54s 6w4usr yu sur"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
 
-save_file(my_list, "add_item.json")
+
+try:
+    existing_items = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    existing_items = []
+
+new_items = sys.argv[1:]
+updated_items = existing_items + new_items
+
+save_to_json_file(updated_items, "add_item.json")
