6929a3f6819a5f4d6dd4144eb515bd3e129196fd
Salma2486 <saloom_001@hotmail.com>
2023-12-05 05:41:30 -0800

    task0

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" y5w4lhi iul4w y6uilhw 64y"""
+
+
+def write_file(filename="", text=""):
+    """uietruit eui t3iuh3t 4ioj t3"""
+    with open(filename, 'w', encoding="utf-8") as file:
+        return file.write(text)
