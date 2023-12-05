5bf2302cf05b15f200c957851837bb5a11b95ebb
Salma2486 <saloom_001@hotmail.com>
2023-12-05 07:05:24 -0800

    task6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""oi jeoihse yoihes ytlijaeryl ijae r"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """oihte oiwe5 yiljwe5ey ioh"""
+    with open(filename, 'r') as file:
+        return json.load(file)
