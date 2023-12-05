c3d7b556ca33c64706130eae3478cb661d368483
Salma2486 <saloom_001@hotmail.com>
2023-12-05 05:30:48 -0800

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """ijow5t oijw45 yoijw4 5y"""
-    with open('filename', 'r', encoding="utf-8") as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         file_data = file.read()
         print(file_data, end='')
