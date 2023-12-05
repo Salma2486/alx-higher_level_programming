a98fee5f74d52b6190a40ae19123fa95070092bf
Salma2486 <saloom_001@hotmail.com>
2023-12-05 07:28:38 -0800

    task7777777777

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-""" Module that adds all arguments to a Python list, and then
-save them to a file
-"""
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-import json
 
-
-def save_to_json_file(my_obj, filename):
-    """ y6re u udr u6d u"""
-    with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(my_obj, file, ensure_ascii=False, indent=2)
-
-
-def load_from_json_file(filename):
-    """ yw54s 6w4usr yu sur"""
-    with open(filename, 'r', encoding='utf-8') as file:
-        return json.load(file)
-
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file_module = __import__('6-load_from_json_file')
+load_from_json_file = load_from_json_file_module.load_from_json_file
 
 try:
     existing_items = load_from_json_file("add_item.json")
