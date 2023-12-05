114894abe1ae53412ad85037251354c684c81eaa
Salma2486 <saloom_001@hotmail.com>
2023-12-05 14:53:10 -0800

    fdgv

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,9 @@ save them to a file
 import sys
 
 
-save_json = __import__('5-save_to_json_file').save_to_json_file
-load_json = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_json = __import__('5-save_to_json_file').save_to_json_file
+    load_json = __import__('6-load_from_json_file').load_from_json_file
 
 file = "add_item.json"
 try:
