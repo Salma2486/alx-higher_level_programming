4143a4394ba6f30f1266b46068fb82bd59a06442
Salma2486 <saloom_001@hotmail.com>
2023-12-11 11:00:27 -0800

    aerdg

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -69,3 +69,51 @@ class Base:
                 return instances
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serializes a list of objects to a CSV file."""
+        filename = f"{cls.__name__}.csv"
+        with open(filename, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            if cls.__name__ == 'Rectangle':
+                for obj in list_objs:
+                    elif cls.__name__ == 'Square':
+                        writer.writerow([
+                            obj.id,
+                            obj.width,
+                            obj.height,
+                            obj.x,
+                            obj.y
+                            ])
+                        for obj in list_objs:
+                            writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserializes objects from a CSV file."""
+        filename = f"{cls.__name__}.csv"
+        objects = []
+        try:
+            with open(filename, 'r') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    if cls.__name__ == 'Rectangle':
+                        obj = cls(
+                                int(row[1]),
+                                int(row[2]),
+                                int(row[3]),
+                                int(row[4]),
+                                int(row[0])
+                                )
+                    elif cls.__name__ == 'Square':
+                        obj = cls(
+                                int(row[1]),
+                                int(row[2]),
+                                int(row[3]),
+                                int(row[0])
+                                )
+                        objects.append(obj)
+        except FileNotFoundError:
+            pass
+        return objects
