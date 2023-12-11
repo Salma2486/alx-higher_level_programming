769b0d16e94765dbd15cd746f84ed85fb9688e66
Salma2486 <saloom_001@hotmail.com>
2023-12-11 10:43:37 -0800

    gtse w tg

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,20 +26,3 @@ class Square(Rectangle):
     def __str__(self):
         """ ey5althje asyo5i;js ryt"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-
-    def update(self, *args, **kwargs):
-        """ib ui hy uhi8h8 8u"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.size = args[1]
-        if len(args) >= 3:
-            self.x = args[2]
-        if len(args) >= 4:
-            self.y = args[3]
-        if kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def to_dictionary(self):
-        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
