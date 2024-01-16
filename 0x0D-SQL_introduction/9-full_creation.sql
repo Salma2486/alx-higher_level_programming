5fb424eddc64d0c33f1d604622f341c5911de7dc
Salma2486 <saloom_001@hotmail.com>
2024-01-16 05:36:37 -0800

    task9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- oleaktwsy hy
+-- srlkth olrs k
+CREATE table if not exists second_table(
+	id int,
+	name varchar(256),
+	score int
+);
+INSERT INTO second_table (id, name, score) values(1, "John", 10)
+INSERT INTO second_table (id, name, score) values(2, "Alex", 3)
+INSERT INTO second_table (id, name, score) values(3, "Bob", 14)
+INSERT INTO second_table (id, name, score) values(4, "George", 8)
