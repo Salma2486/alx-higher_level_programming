33d94cb95433678daa6ae1dbf9e10151f37ba396
Salma2486 <saloom_001@hotmail.com>
2024-01-17 10:45:56 -0800

    task13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,13 @@
 -- sr5t ywr yhw4 t
 -- qa5 eyq45y eqy4
-DELETE FROM second_table WHERE score <= 5;
+SELECT
+tv_show_genres.genre_id AS genre,
+COUNT(tv_shows.id) AS number_of_shows
+FROM
+tv_show_genres
+JOIN
+tv_shows ON tv_show_genres.show_id = tv_shows.id
+GROUP BY
+tv_show_genres.genre_id
+ORDER BY
+number_of_shows DESC;
