8d174dd46efb773b41999825a6c079dc8eda6099
Salma2486 <saloom_001@hotmail.com>
2024-01-17 10:49:46 -0800

    task13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,7 @@
 -- sr5t ywr yhw4 t
 -- qa5 eyq45y eqy4
-SELECT
-tv_show_genres.genre_id AS genre,
-COUNT(tv_shows.id) AS number_of_shows
-FROM tv_show_genres
-JOIN LEFT tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
