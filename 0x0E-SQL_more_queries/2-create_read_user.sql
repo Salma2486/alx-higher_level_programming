eaedcd89e2d97756082cef46ec54835a8c400b80
Salma2486 <saloom_001@hotmail.com>
2024-01-17 06:20:22 -0800

    task2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- rs hy hrs
+-- srth ywsr hy
+CREATE database if not exists hbtn_0d_2;
+CREATE USER if not exists 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT on *.* TO 'user_0d_2'@'localhost';
