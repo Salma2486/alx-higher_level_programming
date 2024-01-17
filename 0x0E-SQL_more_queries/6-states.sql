46b7683e12227ed10572868679ff34a8dd1185ef
Salma2486 <saloom_001@hotmail.com>
2024-01-17 06:50:21 -0800

    task6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- fghghh
+-- hgghhh
+CREATE database if not exists hbtn_0d_usa;
+CREATE table if not exists hbtn_0d_usa.satates(
+	id int unique not null auto_increment primary key,
+	name varchar(256)not null);
+
