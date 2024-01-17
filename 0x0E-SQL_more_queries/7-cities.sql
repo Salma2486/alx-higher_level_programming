5088292bdaa4225b7480b05a834202bba84b52e5
Salma2486 <saloom_001@hotmail.com>
2024-01-17 07:22:24 -0800

    task7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,8 @@
 -- dthyj jed 
 CREATE database if not exists hbtn_0d_usa;
 USE hbtn_0d_usa
-CREATE table if not exists cities(
+CREATE table if not exists hbtn_0d_usa.cities(
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id int not null FOREIGN KEY REFERENCES states(id),
+	state_id int not null,
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
 	name VARCHAR(256) not null);
