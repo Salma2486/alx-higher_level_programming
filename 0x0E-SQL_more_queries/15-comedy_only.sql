6ec0ebff4e4dadbb0fb9d7d2edbdeb5883691d78
Salma2486 <saloom_001@hotmail.com>
2024-01-17 10:52:24 -0800

    task15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- are tr y6tw6 ysr
+-- sr6 u5 ew6uue 5
+SELECT title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+GROUP BY title
+ORDER BY title ASC;
