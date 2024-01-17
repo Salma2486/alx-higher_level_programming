8e901413789163e9da4281fb62c35d9f6bff41c4
Salma2486 <saloom_001@hotmail.com>
2024-01-17 08:09:29 -0800

    task8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- ggtbr trhf 
 -- edfrtg eerg
-SELECT id, name from cities where state_id = (SELECT id from states where name = 'California');
+SELECT id, name from cities where state_id in (SELECT id from states where name = 'California');
