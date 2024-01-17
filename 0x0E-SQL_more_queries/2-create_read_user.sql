ecb0a97874bcb6eefd4ed6c3ffc9ba8e7b2f29ec
Salma2486 <saloom_001@hotmail.com>
2024-01-17 06:22:52 -0800

    task2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,4 @@
 -- srth ywsr hy
 CREATE database if not exists hbtn_0d_2;
 CREATE USER if not exists 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on user_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT on hbtn_0d_2.* TO 'user_0d_2'@'localhost';
