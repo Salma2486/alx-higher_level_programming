cddd8dfaec73df489ceb99266b5336677ca2baaa
Salma2486 <saloom_001@hotmail.com>
2024-01-17 10:30:59 -0800

    task10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 -- ozlkjdfobljk sergtt
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.title = tv_show_genres.genre_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
