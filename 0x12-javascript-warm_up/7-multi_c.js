0151ac875df08bc9d1f301907031b85462c96a98
Salma2486 <saloom_001@hotmail.com>
2024-02-12 09:40:18 -0800

    task7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let arg1 = parseInt(process.argv[2]);
+if (!Number.isNaN(arg1)) {
+  for (let i = 0; i < arg1; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
