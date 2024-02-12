298672dfccea06b7c38b8089f9ef8c7a8792b3c4
Salma2486 <saloom_001@hotmail.com>
2024-02-12 08:27:03 -0800

    task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let arg1 = process.argv[2];
-numarg1 = parseInt(arg1);
+const arg1 = process.argv[2];
+const numarg1 = parseInt(arg1);
 if (Number.isInteger(numarg1)) {
-  console.log('My number: ' + numarg1)
+  console.log('My number: ' + numarg1);
 } else {
   console.log('Not a number');
 }
