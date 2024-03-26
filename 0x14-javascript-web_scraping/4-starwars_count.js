f4f0f87001443858b91d1b5961ec7aaf68297510
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-03-26 14:00:00 +0000

    task4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@ const request = require('request');
 const apiUrl = process.argv[2];
 request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.elog(error);
+    console.log(error);
   }
   const data = JSON.parse(body);
   const films = data.results.filter(film => film.characters.includes('18'));
