5cdd8b91a4c1cb5a95ad8d324c3cec604f0d2cab
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-03-26 13:42:51 +0000

    task3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
