84c459727563998d00f5ee1ceb9377d7527b64e7
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-03-26 14:22:16 +0000

    task4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 const apiUrl = process.argv[2];
+const characterId = '18';
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.log(error);
   }
   const data = JSON.parse(body);
-
-  const films = data.results.filter(film => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
-
-  console.log(films.length);
+  const filmsWithWedge = data.results.filter(film => 
+    film.characters.some(characterUrl => characterUrl.endsWith(`/${characterId}/`))
+  );
+  console.log(filmsWithWedge.length);
 });
