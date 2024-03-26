32cccd648f8c23a8002b262639f271ec3d41e0d7
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-03-26 13:24:50 +0000

    task222

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const request = require('request');
 const url = process.argv[2];
 request.get(url, function (error, response) {
-  if (error) console.log(error); else console.log('code: ', response.statusCode);
+  if (error) console.log(error); else console.log('code:', response.statusCode);
 });
