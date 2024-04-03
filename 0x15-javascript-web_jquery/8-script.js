7ef61e07cac10af1e3ba271ee98f6bf5c8ff62f6
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-04-03 16:42:20 +0000

    t

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+        $.each(data.results, function(i, movie) {
+            $('#list_movies').append('<li>' + movie.title + '</li>');
+        });
+    });
